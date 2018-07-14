0fb72ba081a638c10cf4f274599f63780acd5447
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-14 18:54:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,16 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-WIN_COMBINATIONS = []
+WIN_COMBINATIONS = []WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
 
   WIN_COMBINATIONS.each do |combination|
     puts combination
