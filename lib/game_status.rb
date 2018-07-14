f71a63fe006551526ac0e1096318901e15dd9968
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-14 18:54:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-WIN_COMBINATIONS = []WIN_COMBINATIONS = [
+WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
