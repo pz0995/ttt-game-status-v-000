4c0e4f6f85f614a2e080a93fd2c6d33cf01f3ed0
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-14 23:29:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,22 +19,16 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-
     board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
   end
-
-
-
-
 end
 
 def full?(board)
-  if board.draw
+  if board.all?
     return true
   else !board.won? && board.full?
     return false
   end
-
 end
 
 def draw?(board)
