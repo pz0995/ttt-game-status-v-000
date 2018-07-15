3d595639986fd752ec3768cd7c91db4edd4d9595
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 03:43:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,7 +31,7 @@ def full?(board)
   end
 end
 
-def winner?(board)
+def winner(board)
 #should accept a board and return the token, "X" or "O" that has won the game given a winning board.
 #find winning WIN_COMBINATIONS, use winning index to find X or O
   winning_combination = won?(board)
