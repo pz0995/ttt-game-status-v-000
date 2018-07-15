4b84c11677cd58f1c92c4113051e6357ce91eb27
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-15 03:00:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
   [2, 5, 8]
 ]
 
-# 
+#
 # def won?(board)
 #   WIN_COMBINATIONS.detect do |combo|
 #     board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
